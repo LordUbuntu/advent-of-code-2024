2c4b27282c2a0fceafb7e573ed34f9c99cd1ca61
Jacobus Burger <therealjacoburger@gmail.com>
2024-12-04 18:41:15 -0800

    warning: avoid premature optimization

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
 # Jacobus Burger (2024-12-01)
 # Common utilities for solving problems, including downloading
 #   inputs, problem descriptions, and uploading solutions.
+#
+# Remember: premature optimization is the root of many coding evils
