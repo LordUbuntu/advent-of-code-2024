564029fd7973ec40c774f32987addffdbe94044f
Jacobus Burger <therealjacoburger@gmail.com>
2024-12-05 17:32:22 -0800

    add: by_line input parsing

--- util.py
+++ util.py
@@ -3,3 +3,17 @@
 #   inputs, problem descriptions, and uploading solutions.
 #
 # Remember: premature optimization is the root of many coding evils
+
+
+# best way is to learn from the best, here's some references to look at
+# - https://github.com/nitekat1124/advent-of-code-2024/blob/main/utils/files.py
+# - https://github.com/NadiaMit/AdventOfCode2024/blob/main/helpers/helpers.py
+# - https://github.com/nitekat1124/advent-of-code-2024
+
+
+# A recurring pattern is to read one line at a time, so a generator
+#   is a convenient pattern for parsing inputs
+def by_line(filename: str, strip_chars: str = " ") -> Iterator[str]:
+    with open(filename, 'r') as file:
+        for line in file.readlines():
+            yield line.strip(strip_chars)
