e8941fdc2850f9cda68fef692e107a3c1b814da5
Jacobus Burger <therealjacoburger@gmail.com>
2024-12-06 19:37:07 -0800

    add: noted theory behind day 1 part 1

--- day1/day1.py
+++ day1/day1.py
@@ -1,2 +1,23 @@
 # Jacobus Burger (2024-12-01)
 # Advent Of Code - Day 1
+
+
+# Part 1 - Theory
+# My understanding is we want to take the difference between the left and right
+#   elements of the list once both sides are sorted. Thus we tally the
+#   difference between each list
+# Example:
+# 3   4
+# 4   3
+# 2   5
+# 1   3
+# 3   9
+# 3   3
+#
+# 1   3   d2
+# 2   3   d1
+# 3   3   d0
+# 3   4   d1
+# 3   5   d2
+# 4   9   d5
+# 2 + 1 + 0 + 1 + 2 + 5 = 11
