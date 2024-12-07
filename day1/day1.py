2c4b19fef51a09d3c87a30e8540eee3666802ee4
Jacobus Burger <therealjacoburger@gmail.com>
2024-12-06 19:48:40 -0800

    add: my thoughts and notes about day 1 part 1 solution

--- day1/day1.py
+++ day1/day1.py
@@ -21,3 +21,29 @@
 # 3   5   d2
 # 4   9   d5
 # 2 + 1 + 0 + 1 + 2 + 5 = 11
+#
+# Solution Idea 1:
+# From this, one direct idea is to take the two columns of numbers into their own
+#   seperate lists, sort them, then take their sorted differences like above.
+# Pseudocode 1:
+#   input is array of string
+#   A is array of int
+#   B is array of int
+#   total is int
+#
+#   for a, b in input:
+#     add a to A
+#     add b to B
+#   sort A
+#   sort B
+#   for i in count(input):
+#     total += abs(A[i] - B[i])
+# Thoughts:
+# This is the most obvious solution, though it's likely possible to use a
+#   better approach by sorting inputs as they are received. It doesn't seem
+#   possible to do better than two passes of n for O(2n) however, as the least
+#   number may be at the end of either side of the list, and so it wouldn't
+#   be possible to calculate the running differences without receiving the full
+#   of both lists first. I considered a greedy approach to this, but that would
+#   calculate the wrong differences preemptively. There is no way to preempt
+#   the differences as far as I am aware.
