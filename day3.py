aeb09f3d42052f0f57e23c1ae43c4a79b2a1e681
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-11 23:50:57 -0800

    wrote parser

--- day3.py
+++ day3.py
@@ -1,10 +1,13 @@
 # Jacobus Burger (started 2025-01-11)
 # Day 3
+import re
 
 
 # first part is parsing, pattern as input incase that's changed
 def parse(filename: str, pattern: str) -> list:
-    pass
+    return re.findall(pattern, open(filename, "r").read())
+
+# mul\(\d{1,3}\,\d{1,3}\)
 
 
 # day 3 part 1 is simple, just use regex to match specified pattern,
