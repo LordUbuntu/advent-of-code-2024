187b12bdae40496cdee3d51c809fb40993ddfe38
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-11 23:43:03 -0800

    plan parser

--- day3.py
+++ day3.py
@@ -2,5 +2,10 @@
 # Day 3
 
 
+# first part is parsing, pattern as input incase that's changed
+def parse(filename: str, pattern: str) -> list:
+    pass
+
+
 # day 3 part 1 is simple, just use regex to match specified pattern,
 #   then calculate it
