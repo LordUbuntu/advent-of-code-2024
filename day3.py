9027077de079c4b3e4be35d8c45604f70962382a
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-14 20:56:14 -0800

    not quite right but might be closer

--- day3.py
+++ day3.py
@@ -30,4 +30,20 @@ def part1(filename: str) -> int:
 #   match with this time needs to match between tokens "do()" up until
 #   "don't()"
 def part2(filename: str) -> int:
-    pass
+    # match all the the substrings between "do()" and "don't()"
+    regex = r"(?:do\(\)|^)(.*?)(?:don\'t\(\)|$)"
+    matches = re.findall(regex, open(filename, "r").read())
+    # match all mul operations within those substrings
+    str = ''.join(matches)
+    print(str)
+    regex = r"mul\(\d{1,3}\,\d{1,3}\)"
+    operations = re.findall(regex, str)
+    print(operations)
+    # return the sum like before
+    return sum([    # return sum of
+        n[0] * n[1] # the products of
+        for n in [  # each pair of numbers a,b from inputs "mul(a,b)"
+            list(map(int, operation.strip("mul()").split(',')))
+            for operation in operations
+        ]
+    ])
