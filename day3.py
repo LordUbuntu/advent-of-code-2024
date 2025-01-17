affa676b6824288fc2b4475ef5f2b3cbcae4fa0d
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-16 23:53:26 -0800

    changed day 3, need to test tomorrow

--- day3.py
+++ day3.py
@@ -27,26 +27,27 @@ def part1(filename: str) -> int:
 
 from pprint import pprint  # just to make debug messages nicer
 # PART 2 -
-# day 3 part 2 is abot as simple. The difference is that the regex to
-#   match with this time needs to match between tokens "do()" up until
-#   "don't()"
+# This one was great becaue I learned a ton about regex along the way
+# I initially thought to try creating a regex to only capture substrings
+#   between "do()" and "don't()" but after checking solutions once my
+#   3 day max limit was up, I decided to go with my original idea of
+#   going iteratively and switching between ignoring and accepting "mul"
+#   commands using a flag
 def part2(filename: str) -> int:
-    # match all the the substrings between "do()" and "don't()"
+    total = 0
+    regex = r"do\(\)|don\'t\(\)|mul\(\d{1,3},\d{1,3}\)"
     string = open(filename, "r").read()
-    regex = r"(?:do\(\)|^)(.*?)(?:don\'t\(\)|$)"
-    matches = re.findall(regex, string)
-    pprint(f"{string}\n{regex}\n{matches}\n\n")
-    # match all mul operations within those substrings
-    string = ''.join(matches)
-    regex = r"mul\(\d{1,3}\,\d{1,3}\)"
-    operations = re.findall(regex, string)
-    pprint(f"{string}\n{regex}\n{matches}\n\n")
-    pprint(operations)
-    # return the sum like before
-    return sum([    # return sum of
-        n[0] * n[1] # the products of
-        for n in [  # each pair of numbers a,b from inputs "mul(a,b)"
-            list(map(int, operation.strip("mul()").split(',')))
-            for operation in operations
-        ]
-    ])
+    accepting_commands = True
+    for substring in re.finditer(regex, string):
+        match substring[0]:
+            case "do()":
+                accepting_commands = True
+            case "don't()":
+                accepting_commands = False
+            case _:
+                if accepting_commands:
+                    nums = [
+                        int(num)
+                        for num in substring.strip("mul()").split(',')
+                    ]
+                    total += nums[0] * nums[1]
