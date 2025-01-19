7568d7c0b3980aade2d3aff2bee747b93ad25992
Jacobus Burger <therealjacoburger@gmail.com>
2025-01-18 19:30:18 -0800

    thought for day4

---
+++ day4.py
@@ -0,0 +1,9 @@
+# Jacobus Burger (started 2025-01-18)
+# 
+
+# PART 1 - 
+# So far the problem seems pretty simple. I need to implement a DFS that
+#   starts on any X and goes in one of the 8 grid directions. If it finds
+#   the letters "XMAS" in that order, then after fully exploring that X
+#   mark it as done and move on to the next one. X's are chosen by simply
+#   scanning across the grid.
